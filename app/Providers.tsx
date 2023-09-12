119e2a55c5b5485e34dfa37b33332511417cf2c3
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-09-12 10:46:33 +0700

    feat(tanstack/query): add librairie and setup

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 };
