a7699ccf454e6249956c0bf6533f17c488952c91
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-09-12 11:02:57 +0700

    feat(providers): add providers

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { ToastProvider } from '@/components/ui/toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
 
@@ -8,8 +10,16 @@ const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <SessionProvider>
+          <ToastProvider>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </ToastProvider>
+        </SessionProvider>
+      </ThemeProvider>
+    </>
   );
 };
