b7f4b07b5442ea21f7e07d9dbd15bd4f47443822
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-10-04 11:15:01 +0800

    feat(landing): add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
-import Editor from '@/components/features/mdx/MdxEditor';
-import { Suspense } from 'react';
+import { Typography } from '@/components/ui/Typography';
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function HomePage() {
   return (
-    <div className="m-auto max-w-2xl mt-4">
-      <Suspense fallback={null}>
-        <Editor markdown="# Title" />
-      </Suspense>
+    <div className="flex flex-col gap-4 items-center mt-4">
+      <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-red-400 to-pink-600">
+        YouCode
+      </h1>
+      <Typography variant="lead">
+        The interactive learning platform for developers <i>(only)</i>
+      </Typography>
+      <div className="max-w-4xl m-auto flex gap-4">
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle>Create your own courses</CardTitle>
+            <CardDescription>
+              Quickly create your courses with interactive video and code editor.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle>Follow the courses of creator</CardTitle>
+            <CardDescription>
+              Easily find best courses with the explorer !
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   );
 }
